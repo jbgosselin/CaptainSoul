f0f4c2b2aa88e7f9c8802584c7897d11a7707d64
Jean-Baptiste Gosselin <mrknacky@gmail.com>
2013-06-05 16:19:29 +0200

    stating logging

--- captainsoul/main.py
+++ captainsoul/main.py
@@ -7,10 +7,25 @@ from twisted.internet import reactor
 from gi.repository import Notify
 Notify.init("CaptainSoul")
 
+import logging
+
+from CmdLine import options
+
 from MainWindow import MainWindow
 
 
+def configLogging():
+    fmt = '%(levelname)s:%(message)s'
+    level = logging.WARNING
+    if options.log_debug:
+        level = logging.DEBUG
+    elif options.log_info:
+        level = logging.INFO
+    logging.basicConfig(level=level, format=fmt)
+
+
 def start():
+    configLogging()
     win = MainWindow()
     id(win)
     reactor.run()
