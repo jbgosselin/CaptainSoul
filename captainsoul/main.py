62ab50a076605a1bc3ce65085fdf7e269cffcaf1
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-05 15:21:41 +0200

    better gtk integration

--- captainsoul/main.py
+++ captainsoul/main.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import CmdLine
-CmdLine._void()
+from twisted.internet import gtk3reactor
+gtk3reactor.install()
+from twisted.internet import reactor
+
+from gi.repository import Notify
+Notify.init("CaptainSoul")
+
+from MainWindow import MainWindow
 
 
 def start():
-    from twisted.internet import gtk3reactor
-    gtk3reactor.install()
-    from twisted.internet import reactor
-    from MainWindow import MainWindow
-    from gi.repository import Notify
-    Notify.init("CaptainSoul")
     win = MainWindow()
     id(win)
     reactor.run()
