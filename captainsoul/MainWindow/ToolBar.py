534d266f7807723e3a3cc498ae9b285a4c355f7d
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-06 15:30:47 +0200

    better toolbutton

--- captainsoul/MainWindow/ToolBar.py
+++ captainsoul/MainWindow/ToolBar.py
@@ -9,23 +9,23 @@ class ToolBar(Gtk.Toolbar):
         self._mw = mw
         items = []
         # Connection/Deconnection Button
-        self._coButton = Gtk.ToolButton(Gtk.STOCK_CONNECT, tooltip_text="Connect")
+        self._coButton = Gtk.ToolButton(stock_id=Gtk.STOCK_CONNECT, tooltip_text="Connect")
         self._coButtonClicked = self._coButton.connect("clicked", self._mw.connectEvent)
         items.append(self._coButton)
         # Settings Button
-        button = Gtk.ToolButton(Gtk.STOCK_PREFERENCES, tooltip_text="Settings")
+        button = Gtk.ToolButton(stock_id=Gtk.STOCK_PREFERENCES, tooltip_text="Settings")
         button.connect("clicked", self._mw.settingsEvent)
         items.append(button)
         # Separator
         items.append(Gtk.SeparatorToolItem())
         # Add Contact Button
-        button = Gtk.ToolButton(Gtk.STOCK_ADD, tooltip_text="Add contact")
+        button = Gtk.ToolButton(stock_id=Gtk.STOCK_ADD, tooltip_text="Add contact")
         button.connect("clicked", self._mw.addContactWindowEvent)
         items.append(button)
         # Separator
         items.append(Gtk.SeparatorToolItem())
         # Quit Button
-        button = Gtk.ToolButton(Gtk.STOCK_QUIT, tooltip_text="Quit")
+        button = Gtk.ToolButton(stock_id=Gtk.STOCK_QUIT, tooltip_text="Quit")
         button.connect("clicked", self._mw.quitEvent)
         items.append(button)
         # Insert all in the ToolBar
