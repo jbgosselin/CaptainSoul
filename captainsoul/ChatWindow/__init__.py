9b11262efa9cf860c858bb3910973f8135a3c89a
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-08 02:51:25 +0200

    add ChatStatus

--- captainsoul/ChatWindow/__init__.py
+++ captainsoul/ChatWindow/__init__.py
@@ -4,18 +4,17 @@ import gtk
 
 from .. import Icons
 from ChatView import ChatView
+from ChatStatus import ChatStatus
 
 
 class ChatWindow(gtk.Window):
     def __init__(self, manager, login, iconify):
         super(ChatWindow, self).__init__()
         self.set_properties(title="CaptainSoul - %s" % login, icon=Icons.shield.get_pixbuf())
-        self._typing, self._manager, self._login = False, manager, login
+        self._typing = False
         self._createUi(manager, login)
-        self.connect("delete-event", self.deleteEvent)
+        self.connect("delete-event", self.deleteEvent, manager, login)
         self.connect("delete-event", manager.closeChatWindowEvent, login)
-        manager.connect('is-typing', self.isTypingEvent)
-        manager.connect('cancel-typing', self.cancelTypingEvent)
         self.resize(200, 200)
         if iconify:
             self.iconify()
@@ -27,43 +26,34 @@ class ChatWindow(gtk.Window):
         # chatview
         box.add(ChatView(manager, login))
         # is typing bar
-        self._status = gtk.Statusbar()
-        box.pack_start(self._status, False, False, 0)
+        box.pack_start(ChatStatus(manager, login), False, False, 0)
         # user entry
         view = gtk.TextView()
         view.set_properties(editable=True, cursor_visible=True, wrap_mode=gtk.WRAP_WORD_CHAR)
         self._entry = view.get_buffer()
         view.set_size_request(100, 30)
-        view.connect("key-press-event", self.keyPressEvent)
-        self._entry.connect("changed", self.keyPressEventEnd)
+        view.connect("key-press-event", self.keyPressEvent, manager, login)
+        self._entry.connect("changed", self.keyPressEventEnd, manager, login)
         box.pack_start(view, False, False, 0)
 
-    def deleteEvent(self, widget, reason):
+    def deleteEvent(self, widget, reason, manager, login):
         if self._typing:
             self._typing = False
-            self._manager.sendCancelTyping([self._login])
+            manager.sendCancelTyping([login])
 
-    def keyPressEvent(self, widget, event):
-        if gtk.gdk.keyval_name(event.keyval) == 'Return':
+    def keyPressEvent(self, widget, event, manager, login):
+        if gtk.gdk.keyval_name(event.keyval) in ('Return', 'KP_Enter'):
             text = self._entry.get_text(self._entry.get_start_iter(), self._entry.get_end_iter(), True)
             if len(text):
                 self._entry.delete(self._entry.get_start_iter(), self._entry.get_end_iter())
-                self._manager.sendMsg(text, [self._login])
+                manager.sendMsg(text, [login])
             return True
 
-    def keyPressEventEnd(self, widget):
+    def keyPressEventEnd(self, widget, manager, login):
         l = len(self._entry.get_text(self._entry.get_start_iter(), self._entry.get_end_iter(), True))
         if not self._typing and l >= 5:
-            self._manager.sendStartTyping([self._login])
+            manager.sendStartTyping([login])
             self._typing = True
         elif self._typing and l < 5:
-            self._manager.sendCancelTyping([self._login])
+            manager.sendCancelTyping([login])
             self._typing = False
-
-    def isTypingEvent(self, widget, info):
-        if info.login == self._login:
-            self._status.push(0, "Is typing...")
-
-    def cancelTypingEvent(self, widget, info):
-        if info.login == self._login:
-            self._status.remove_all(0)
