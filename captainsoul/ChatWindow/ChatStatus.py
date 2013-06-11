ef8d212774002492023017d1d457cb586a034718
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-11 17:51:54 +0200

    disconnect signal that should be disconnected

--- captainsoul/ChatWindow/ChatStatus.py
+++ captainsoul/ChatWindow/ChatStatus.py
@@ -6,8 +6,11 @@ import gtk
 class ChatStatus(gtk.Statusbar):
     def __init__(self, manager, login):
         super(ChatStatus, self).__init__()
-        manager.connect('is-typing', self.isTypingEvent, login)
-        manager.connect('cancel-typing', self.cancelTypingEvent, login)
+        self.connect('destroy', self.destroyEvent, manager)
+        self._connections = [
+            manager.connect('is-typing', self.isTypingEvent, login),
+            manager.connect('cancel-typing', self.cancelTypingEvent, login)
+        ]
 
     def isTypingEvent(self, widget, info, login):
         if info.login == login:
@@ -16,3 +19,7 @@ class ChatStatus(gtk.Statusbar):
     def cancelTypingEvent(self, widget, info, login):
         if info.login == login:
             self.remove_all(0)
+
+    def destroyEvent(self, widget, manager):
+        for co in self._connections:
+            manager.disconnect(co)
