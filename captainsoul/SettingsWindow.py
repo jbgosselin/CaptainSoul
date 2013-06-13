ad9799e803c1bd705c70eeb544648613a9eedf9c
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-13 19:24:41 +0200

    add disable notification

--- captainsoul/SettingsWindow.py
+++ captainsoul/SettingsWindow.py
@@ -21,7 +21,8 @@ class SettingsWindow(gtk.Dialog):
             'login': self._entries['login'].get_text(),
             'password': self._entries['password'].get_text(),
             'location': self._entries['location'].get_text(),
-            'autoConnect': self._autoButton.get_active()
+            'autoConnect': self._autoButton.get_active(),
+            'notification': self._notiButton.get_active()
         }
 
     def _createUi(self):
@@ -31,7 +32,7 @@ class SettingsWindow(gtk.Dialog):
             entry.set_text(Config[key])
         self._entries['password'].set_visibility(False)
         # create table and populate it
-        table = gtk.Table(4, 3, True)
+        table = gtk.Table(5, 3, True)
         # login
         table.attach(gtk.Label("Login:"), 0, 1, 0, 1)
         table.attach(self._entries['login'], 1, 3, 0, 1)
@@ -46,5 +47,11 @@ class SettingsWindow(gtk.Dialog):
         self._autoButton = gtk.CheckButton()
         self._autoButton.set_active(Config['autoConnect'])
         table.attach(self._autoButton, 2, 3, 3, 4)
+        # notification
+        table.attach(gtk.Label("Notification:"), 0, 2, 4, 5)
+        self._notiButton = gtk.CheckButton()
+        self._notiButton.set_active(Config['notification'])
+        table.attach(self._notiButton, 2, 3, 4, 5)
+        # add to window
         self.vbox.pack_start(table, True, True, 0)
         self.add_buttons("Apply", gtk.RESPONSE_APPLY, "Cancel", gtk.RESPONSE_CANCEL)
