c56c471e258a903fd2ab9f2f06a401779bd11148
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-06-14 18:11:51 +0200

    fix labels in toolbar

--- captainsoul/MainWindow/ToolBar.py
+++ captainsoul/MainWindow/ToolBar.py
@@ -9,31 +9,46 @@ class ToolBar(gtk.Toolbar):
         items = []
         # Connection/Deconnection Button
         coButton = gtk.ToolButton(gtk.STOCK_CONNECT)
-        coButton.set_tooltip_text("Connect")
+        coButton.set_properties(
+            tooltip_text="Connect",
+            label="Connect"
+        )
         self._coButtonClicked = coButton.connect("clicked", manager.connectEvent)
         items.append(coButton)
         # Settings Button
         button = gtk.ToolButton(gtk.STOCK_PREFERENCES)
-        button.set_tooltip_text("Settings")
+        button.set_properties(
+            tooltip_text="Settings",
+            label="Settings"
+        )
         button.connect("clicked", manager.openSettingsWindowEvent)
         items.append(button)
         # Separator
         items.append(gtk.SeparatorToolItem())
         # Add Contact Button
         button = gtk.ToolButton(gtk.STOCK_ADD)
-        button.set_tooltip_text("Add contact")
+        button.set_properties(
+            tooltip_text="Add contact",
+            label="Add contact"
+        )
         button.connect("clicked", manager.openAddContactWindowEvent)
         items.append(button)
         # Open download manager
         button = gtk.ToolButton(gtk.STOCK_SAVE)
-        button.set_tooltip_text("Downloads")
+        button.set_properties(
+            tooltip_text="Downloads",
+            label="Downloads"
+        )
         button.connect("clicked", self.openDownloadEvent, manager)
         items.append(button)
         # Separator
         items.append(gtk.SeparatorToolItem())
         # Quit Button
         button = gtk.ToolButton(gtk.STOCK_QUIT)
-        button.set_tooltip_text("Quit")
+        button.set_properties(
+            tooltip_text="Quit",
+            label="Quit"
+        )
         button.connect("clicked", manager.quitEvent)
         items.append(button)
         # Insert all in the ToolBar
@@ -46,13 +61,19 @@ class ToolBar(gtk.Toolbar):
 
     def connectedEvent(self, widget, button, manager):
         button.set_stock_id(gtk.STOCK_DISCONNECT)
-        button.set_tooltip_text("Disconnect")
+        button.set_properties(
+            tooltip_text="Disconnect",
+            label="Disconnect"
+        )
         button.disconnect(self._coButtonClicked)
         self._coButtonClicked = button.connect("clicked", manager.disconnectEvent)
 
     def disconnectedEvent(self, widget, button, manager):
         button.set_stock_id(gtk.STOCK_CONNECT)
-        button.set_tooltip_text("Connect")
+        button.set_properties(
+            tooltip_text="Connect",
+            label="Connect"
+        )
         button.disconnect(self._coButtonClicked)
         self._coButtonClicked = button.connect("clicked", manager.connectEvent)
 
