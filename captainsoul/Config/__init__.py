b9c40d5d4ac66c58c8aac19c5bef2d74e2f2a4bb
Jean-Baptiste Gosselin <mrknacky@gmail.com>
2013-06-15 13:19:09 +0200

    better file resolution configfile

--- captainsoul/Config/__init__.py
+++ captainsoul/Config/__init__.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+import platform
+import user
+
 from ConfigFile import ConfigFile
 
 __all__ = ['Config']
 
-Config = ConfigFile()
+
+def createConfigFile():
+    if platform.system() == 'Linux':
+        directory = os.path.join(user.home, '.config')
+    else:
+        directory = './'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return ConfigFile(os.path.join(directory, 'cptsoul.json'))
+Config = createConfigFile()
