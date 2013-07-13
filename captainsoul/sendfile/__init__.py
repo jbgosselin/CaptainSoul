f7f6562312a379b537b6d1c914b7e37e480d7134
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2013-07-13 19:19:12 +0200

    totaly changed

--- captainsoul/sendfile/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from twisted.internet import reactor
-
-from captainsoul.sendfile.factory import SendFactory
-
-
-def sendFile(path, ip, port, progCallback, endCallback, errCallback):
-    reactor.connectTCP(ip, port, SendFactory(path, progCallback, endCallback, errCallback))
