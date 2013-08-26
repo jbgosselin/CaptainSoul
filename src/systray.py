4c0bc8992cc6dfe2583c01aa73ea07726885758f
Jean-Baptiste Gosselin <dennajort@gmail.com>
2013-08-26 04:21:06 +0200

    add pynotify alternative with dbus

--- src/systray.py
+++ src/systray.py
@@ -1,26 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import logging
-import platform
-
 import gtk
-if platform.system() == 'Linux':
-    try:
-        import pynotify
-    except ImportError:
-        pynotify = None
-else:
-    pynotify = None
-from glib import GError
 
 import Icons
 from common import CptCommon
+from notify import Notifier
 
 
 class Systray(gtk.StatusIcon, CptCommon):
     def __init__(self):
         super(Systray, self).__init__()
         self._reconnecting = False
+        self._notifier = Notifier()
         self.set_from_pixbuf(Icons.shield)
         self.set_properties(
             tooltip_text="CaptainSoul",
@@ -31,23 +22,14 @@ class Systray(gtk.StatusIcon, CptCommon):
         self.manager.connect('logged', self.loggedEvent)
         self.manager.connect('reconnecting', self.reconnectingEvent)
 
-    def doNotify(self, head, body, img, timeout):
-        if pynotify is not None and self.config["notification"]:
-            notif = pynotify.Notification(head, body, img)
-            notif.set_timeout(timeout)
-            try:
-                notif.show()
-            except GError:
-                logging.warning('Systray : Notification fail')
-
     def msgEvent(self, widget, info, msg, dests):
-        self.doNotify("CaptainSoul - New message from %s" % info.login, msg, 'dialog-information', 5000)
+        self._notifier.notify("CaptainSoul - New message from %s" % info.login, msg, 'dialog-information')
 
     def loggedEvent(self, widget):
         self._reconnecting = False
-        self.doNotify('CaptainSoul', 'Connected', 'dialog-ok', 3000)
+        self._notifier.notify('CaptainSoul', 'Connected', 'dialog-ok')
 
     def reconnectingEvent(self, widget):
         if not self._reconnecting:
-            self.doNotify('CaptainSoul', 'Connection lost, reconnecting', 'dialog-warning', 3000)
+            self._notifier.notify('CaptainSoul', 'Connection lost, reconnecting', 'dialog-warning')
             self._reconnecting = True
