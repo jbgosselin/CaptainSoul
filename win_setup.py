0e0a0799c1b75f255437f06bbbb7085e30223829
Jean-Baptiste Gosselin <dennajort@gmail.com>
2013-08-28 00:57:03 +0200

    remove tab in .py file

--- win_setup.py
+++ win_setup.py
@@ -4,35 +4,34 @@ import os
 import shutil
 
 def packages():
-	p = ['']
-	for dirname, dirnames, filenames in os.walk('src'):
-		if dirname != 'src':
-			dirname = dirname[4:]
-			p.append(dirname)
-	return p
+    p = ['']
+    for dirname, dirnames, filenames in os.walk('src'):
+        if dirname != 'src':
+            dirname = dirname[4:]
+            p.append(dirname)
+    return p
 
 setup(
-	zipfile = None,
-	windows = [
-		{
-			'script': 'src/__main__.py',
-		}
-	],
-	package_dir = {'': 'src'},
-	packages = packages(),
-	options = {
-		'py2exe': {
-			'bundle_files': 3,
-			'compressed': True,
-			'optimize': 2,
-			# Optionally omit gio, gtk.keysyms, and/or rsvg if you're not using them
-			'includes': 'cairo, pango, pangocairo, atk, gobject, gio',
-			'dll_excludes': [
-				"mswsock.dll",
-				"powrprof.dll"
-			],
-		}
-	}
+    zipfile = None,
+    windows = [
+        {
+            'script': 'src/__main__.py',
+        }
+    ],
+    package_dir = {'': 'src'},
+    packages = packages(),
+    options = {
+        'py2exe': {
+            'bundle_files': 3,
+            'compressed': True,
+            'optimize': 2,
+            'includes': 'cairo, pango, pangocairo, atk, gobject, gio',
+            'dll_excludes': [
+                "mswsock.dll",
+                "powrprof.dll"
+            ],
+        }
+    }
 )
 
 shutil.move('dist/__main__.exe', 'dist/cptsoul.exe')
