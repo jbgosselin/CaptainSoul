216bb51163e750813c1caf8c05824a2be76e0f43
Jean-Baptiste Gosselin <dennajort@gmail.com>
2013-09-04 17:50:42 +0200

    add install mode in cptsoul

--- src/__main__.py
+++ src/__main__.py
@@ -2,16 +2,10 @@
 
 import os
 import sys
-
-from twisted.internet import gtk2reactor
-gtk2reactor.install()
-from twisted.internet import reactor
-from twisted.python import log
-observer = log.PythonLoggingObserver()
-observer.start()
-
+import shutil
 import logging
 from argparse import ArgumentParser
+
 from common import CptCommon
 
 
@@ -20,8 +14,8 @@ def get_args():
     parser.add_argument('-v', '--verbose', action='count', dest='verbose', help='Set verbose mode', default=0)
     parser.add_argument('-t', action='store_true', dest='tray', help='Start in tray')
     parser.add_argument('-d', action='store_true', dest='debug', help='Start with debug window')
+    parser.add_argument('--install', action='store_true', dest='install', help='Start in install mode')
     return parser.parse_args()
-CptCommon.cmdline = get_args()
 
 
 def configLogging():
@@ -30,20 +24,55 @@ def configLogging():
     if CptCommon.cmdline.verbose <= 3:
         level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][CptCommon.cmdline.verbose]
     logging.basicConfig(level=level, format=fmt)
-configLogging()
-
-from config import createConfigFile
-CptCommon.config = createConfigFile()
-
-from manager import Manager
 
 
-def main():
+def normal_mode():
+    # imports
+    from twisted.internet import gtk2reactor
+    gtk2reactor.install()
+    from twisted.internet import reactor
+    from twisted.python import log
+    observer = log.PythonLoggingObserver()
+    observer.start()
+    from config import createConfigFile
+    from manager import Manager
+    # start
+    configLogging()
+    CptCommon.config = createConfigFile()
     manager = Manager()
     manager()
     reactor.run()
     if CptCommon.willReboot:
         os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+def install_mode():
+    def try_to_import(module):
+        try:
+            __import__(module)
+            return True
+        except:
+            return False
+
+    modules = filter(try_to_import, ['twisted', 'pygtk'])
+    if not modules:
+        print "Missing module: %s." % ', '.join(modules)
+        exit(1)
+    if os.getuid() == 0:
+        name = os.path.dirname(os.path.abspath(__file__))
+        shutil.copy(name, os.path.join('/usr/bin', 'cptsoul'))
+        print "Successfully installed"
+    else:
+        print "You must be root/sudo in order to install."
+        exit(1)
+
+
+def main():
+    CptCommon.cmdline = get_args()
+    if CptCommon.cmdline.install:
+        install_mode()
+    else:
+        normal_mode()
 
 if __name__ == '__main__':
     main()
