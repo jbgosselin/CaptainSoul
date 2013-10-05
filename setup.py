751880754a47c9123bd7d52cce8543faff58342d
Jean-Baptiste Gosselin <dennajort@gmail.com>
2013-10-06 01:20:33 +0200

    v0.1.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CaptainSoul",
-    version="0.1.6",
+    version="0.1.7",
     author="gossel_j",
     author_email="gosselinjb@gmail.com",
     description="Netsoul client with pygtk and twisted",
